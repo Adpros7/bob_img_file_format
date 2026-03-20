c5e6e95bc3627e21e93ce10035bc9e602cdeacd4
Adpros7 <pranit.advik@gmail.com>
2026-03-19 20:59:31 -0400

    Move image files into examples directory and update main.py to compress output

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import zlib
 from PIL import Image
 
 
-def find_repeated(s, threshold=5, size=5):
+def find_repeated(s, threshold=5, size=7):
     matches = re.findall(rf"\d{{{size}}}", s) + re.findall(rf"\w{{{size}}}", s)
     counts = Counter(matches)
     return {k: v for k, v in counts.items() if v > threshold}
@@ -25,13 +25,13 @@ def main(image: str):
     for i, v in enumerate(find_repeated(final).items()):
         final = f"{chr(i + 122)}={v[0]}{final}"
 
-    # final = lzma.compress(final.encode("utf-8"))
+    final = lzma.compress(final.encode("utf-8"))
 
-    # with open("new.bob", "wb") as f:
-    #     f.write(final)
-
-    with open("newtxt.bob", "w", encoding="utf-8") as f:
+    with open("new.bob", "wb") as f:
         f.write(final)
+
+    # with open("newtxt.bob", "w", encoding="utf-8") as f:
+    #     f.write(final)
 if __name__ == "__main__":
-    main("pngwing.com.png")
-    # main("img.jpg")
+    # main("examples/pngwing.com.png")
+    main("img.jpg")
--- messup.py
+++
@@ -1,6 +0,0 @@
-with open("img.jpg", "rb") as f:
-    og = f.read()
-
-og = og[5757] = b"!"
-with open("new.jpg", "wb") as f:
-    f.write(b"kjdhauwejsdlfjer9iur4-===42eworf=+++P{WQKorke]]||{|P{k;odsjfpe}}}" + og + b"383q023rrd||||||||||||||}{{}QW~L!<LKMWS!KNQHUVQRDZ^Q%&^Q}")
