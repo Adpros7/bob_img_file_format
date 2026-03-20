8958a3eaebcab1e907504087bd5af2e7eb7feb65
Adpros7 <pranit.advik@gmail.com>
2026-03-19 20:55:38 -0400

    Initialize project with Python setup, image processing script, and gitignore

---
+++ main.py
@@ -0,0 +1,34 @@
+import lzma
+import re
+from typing import Counter
+import zlib
+
+from PIL import Image
+
+
+def find_repeated(s, threshold=5, size=5):
+    matches = re.findall(rf"\d{{{size}}}", s) + re.findall(rf"\w{{{size}}}", s)
+    counts = Counter(matches)
+    return {k: v for k, v in counts.items() if v > threshold}
+
+
+def main(image: str):
+    img = Image.open(image).convert("RGBA")
+    rgb_to_hex = lambda r, g, b, a: f"{"#" if a < 255 else ""}{r:02x}{g:02x}{b:02x}" + (f"{a:02x}" if a < 255 else "")
+    final = ""
+    for i in range(img.width):
+        for j in range(img.height):
+            final += rgb_to_hex(*img.getpixel((i, j))) # type: ignore
+            # print(img.getpixel((i, j)))
+
+
+    # for i, v in enumerate(find_repeated(final).items()):
+    #     final = final.replace(i, f"<{v}>")
+
+    final = lzma.compress(final.encode("utf-8"))
+
+    with open("new.bob", "wb") as f:
+        f.write(final)
+if __name__ == "__main__":
+    # main("pngwing.com.png")
+    main("img.jpg")
